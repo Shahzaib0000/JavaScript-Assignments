df21604b7fbd0a64168970907553edbda2e31ad4
= <shahzaibawan085@gmail.com>
2021-10-01 16:00:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// ******** Chapter 01-03 *************
+// ******** Alerts, Variables of Strings, Variables of Numbers********
+
+// ******* Task No: 01 *****
+
+// alert("Assalam-o-Alaikum")
+
+// ******* Task No: 02 *****
+
+// alert("Hello Mr.Been." + "\n"+ "How are you?")
+
+
+// ******* Task No: 03 *****
+
+// var message = "Hello World"
+// alert(message)
+
+// ******* Task No: 04 *****
+
+// var book = "A smart way to learnt JavaScript "
+// document.write(book)
+
+// ******* Task No: 05 *****
+
+// var age = "I am 21 Years old"
+// alert(age)
+
+// ******* Task No: 05 *****
+
+// var birthYear = "My birth year is 2000"
+// alert(birthYear)
+
+
